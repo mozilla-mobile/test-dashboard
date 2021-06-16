8330a4f816dd929a34f50f13a97d4d3f3185bd3e
Richard Pappalardo <rpappalax@gmail.com>
2021-06-16 13:03:14 -0700

    MVP for database cron job (#12)
    * replace old with new
    
    * remove googlecloudrun
    
    * fix: yml
    
    * fix: yml2
    
    * fix: yml3
    
    * fix: yml4
    
    * fix: yml4
    
    * fix: yml5
    
    * fix: env vars
    
    * fix: env vars
    
    * fix: cloud-sql-proxy conn
    
    * fix: cloud-sql-proxy conn
    
    * fix: cloud-sql-proxy conn
    
    * fix: cloud-sql-proxy conn
    
    * fix: cloud-sql-proxy conn
    
    * fix: cloud-sql-proxy conn
    
    * feat: move to cron
    
    * fix: lint
    
    * fix: lint
    
    * fix: pr review

---
+++ base.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+"""Base database connector module
+https://github.com/swaathi/sqlalchemy"""
+
+import os
+
+from sqlalchemy import create_engine, engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+db_username = os.environ['CLOUD_SQL_DATABASE_USERNAME']
+db_password = os.environ['CLOUD_SQL_DATABASE_PASSWORD']
+db_name = os.environ['CLOUD_SQL_DATABASE_NAME']
+db_host = os.environ.get('CLOUD_SQL_DATABASE_HOST', 'localhost')
+db_port = os.environ.get('CLOUD_SQL_DATABASE_PORT', '3307')
+db_config = {
+    "pool_size": 5,
+    "max_overflow": 2,
+    "pool_timeout": 30,  # 30 seconds
+    "pool_recycle": 1800,  # 30 minutes
+}
+
+Base = declarative_base()
+
+pool = create_engine(
+    engine.url.URL(
+        drivername="mysql+pymysql",
+        username=db_username,
+        password=db_password,
+        host=db_host,
+        port=db_port,
+        database=db_name,
+    ),
+    **db_config
+)
+
+
+Session = sessionmaker(bind=pool)
+Base.metadata.bind = pool
