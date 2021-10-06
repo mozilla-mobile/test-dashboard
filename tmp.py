7b5abd849d45a30946e58d7a84ffb2d0f02e2a38
Richard Pappalardo <rpappalax@gmail.com>
2021-10-06 21:59:15 +0200

    test workflow (WIP)

--- tmp.py
+++ tmp.py
@@ -1,8 +1,7 @@
 import logger
 
 
-
-_logger = logging.getLogger('yo')
 print('WIP: print from print')
 
- _logger.debug("WIP: print from logger")
+_logger = logging.getLogger('yo')
+_logger.debug("WIP: print from logger")
