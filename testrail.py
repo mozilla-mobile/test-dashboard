4abf307b76a447e832a36332885cafe12715fe0d
Richard Pappalardo <rpappalax@gmail.com>
2021-10-08 23:15:43 +0200

    Test run, count test exec by date range (WIP)

--- testrail.py
+++ testrail.py
@@ -47,8 +47,16 @@ class TestRail:
         return self.client.send_get('get_suite/{0}'.format(suite_id))
 
     # API: Runs
-    def test_runs(self, project_id):
-        return self.client.send_get('get_runs/{0}'.format(project_id))
+    def test_runs(self, project_id, start_date='', end_date=''):
+        date_range = ''
+        if start_date:
+            after = self.convert_datetime_to_epoch(start_date)
+            date_range += '&created_after={0}'.format(after)
+        if end_date:
+            before = self.convert_datetime_to_epoch(end_date)
+            date_range += '&created_before={0}'.format(before)
+        #return self.client.send_get('get_runs/{0}'.format(project_id))
+        return self.client.send_get('get_runs/{0}{1}'.format(project_id, date_range))
 
     def test_run(self, run_id):
         return self.client.send_get('get_run/{0}'.format(run_id))
@@ -56,21 +64,29 @@ class TestRail:
     def test_results_for_run(self, run_id):
         return self.client.send_get('get_results_for_run/{0}'.format(run_id))
 
+    # UTILITIES
+    def convert_datetime_to_epoch(self, str_date):
+        from datetime import datetime
+
+        p = '%Y-%m-%dT%H:%M:%S.%fZ'
+        mytime = "{0}T00:00:01.000Z".format(str_date)
+        epoch = datetime(1970, 1, 1)
+        tmp = (datetime.strptime(mytime, p) - epoch).total_seconds()
+        return int(round(tmp))
+
+
 class TestRailHelpers():
-    """
-    mobile_projects = [
-        'Firefox for Android',
-        'Fenix Browser',
-        'Focus for Android',
-        'Firefox for iOS',
-        'Focus for iOS',
-        'Reference Browser'
-    ]
-    """
 
     def __init__(self):
         self.testrail = TestRail()
         self.db = Database()
+
+    def date_range_days(num_days):
+        # TODO given a range, return a tuple with start_date, end_date for 
+        # last 24 hours, last 7 days, etc
+        # will need a more intelligent method to restore historic data
+        # since the data will be on a given datetime stamp we cloud "group" weekly data and 
+        # insert it with a simulated historic created_at date
 
     def testrail_coverage_update(self, project):
         projects_id, testrail_project_id, functional_test_suite_id = self.db.testrail_identity_ids(project) # noqa 
@@ -80,47 +96,29 @@ class TestRailHelpers():
         self.db.report_test_coverage_insert(projects_id, totals)
 
     def testrail_run_update(self, project):
+        # Sample Testrail data from one run:
+        # [{'run_id': 44113}, {'project_id': 59}, {'suite_id': 3192}, {'name': 'Smoke and sanity automated tests - Beta 90.0.0-beta.2'}, {'created_on': 1623151551}, {'completed_on': 1623158050}, {'failed_count': 0}, {'passed_count': 35}, {'retest_count': 0}, {'blocked_count': 0}, {'untested_count': 0}, {'untested_count': 0}] # noqa
+        start_date = '2021-08-01'
+        end_date = '2021-10-31'
         totals = []
+        results = []
         projects_id, testrail_project_id, functional_test_suite_id = self.db.testrail_identity_ids(project) # noqa 
-        runs = self.testrail.test_runs(testrail_project_id)
+        #runs = self.testrail.test_runs(testrail_project_id)
+        runs = self.testrail.test_runs(testrail_project_id, start_date, end_date)
+        result_count = 0
         for run in runs:
             total = self.db.report_test_run_total(run)
             run_id = total[0]['run_id']
-            # [{'run_id': 44113}, {'project_id': 59}, {'suite_id': 3192}, {'name': 'Smoke and sanity automated tests - Beta 90.0.0-beta.2'}, {'created_on': 1623151551}, {'completed_on': 1623158050}, {'failed_count': 0}, {'passed_count': 35}, {'retest_count': 0}, {'blocked_count': 0}, {'untested_count': 0}, {'untested_count': 0}]
             results = self.testrail.test_results_for_run(run_id)
             for result in results:
-                if result['status_id'] in [6]:
-                    #print('RESULT: {0}'.format(result))
-                    print('RESULT_ID: {0}'.format(result['id']))
-                    print('STATUS_ID: {0}'.format(result['status_id']))
-                    print('TEST_ID: {0}'.format(result['test_id']))
-                    print('------------------------------')
-                    #for test in tests:
+                result_count += 1
             # need to add but total a
             totals.append(results)
+
+        # DIAGNOSTIC
+        totals_count = len(totals)
+        print('TOTALS_COUNT: {0}'.format(totals_count))
+        testcase_count = len(results)
+        print('RESULT_COUNT: {0}'.format(result_count))
         sys.exit()
         self.db.report_test_runs_insert(projects_id, totals)
-
-    """
-    def project_ids(self, projects=mobile_projects):
-        # Note: testrail project ids are also stored in database for
-        # convenience as they can't be changed
-        p = self.testrail.projects()
-        results = [item for item in p if any(k in item['name'] for k in projects)] # noqa
-
-        project_arr = []
-        tmp = []
-        for result in results:
-            tmp.append(result['name'])
-            tmp.append(result['id'])
-            project_arr.append(tmp)
-            tmp = []
-
-        return project_arr
-
-    def project_id(self, project_name):
-        # give a project name (as stored in Testrail) and
-        # return testrail project_id
-        return self.project_ids([project_name])[0][1]
-    """
-
