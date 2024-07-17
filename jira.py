39515a9e1a91a1505cefd8fcaaa582251ba23103
isabelrios <isabelrios@gmail.com>
2024-07-17 12:06:01 +0200

    fix insertion and delete method

--- jira.py
+++ jira.py
@@ -26,7 +26,6 @@ class Jira:
 
     def __init__(self):
         try:
-
             JIRA_HOST = os.environ['JIRA_HOST']
             self.client = JiraAPIClient(JIRA_HOST)
             self.client.user = os.environ['JIRA_USER']
@@ -55,62 +54,7 @@ class JiraClient(Jira):
         payload = self.filters()
         print("This is the payload returning from filter")
         print(payload)
-        '''
-        Working Test Payload
-        payload = [{
-            "key": "QA-2555",
-            "fields": {
-                "summary": "[QA for] password generator Android",
-                "created": "2024-07-01T19:00:26.017-0400",
-                "assignee": "None",
-                "customfield_10155": {
-                    "value": "Fx129"
-                },
-                "customfield_10134": {
-                    "value": "Mobile Android"
-                },
-                "customfield_10037": "2",
-                "status": {
-                    "name": "Created"
-                },
-                "labels": [
-                ]
-            }},{
-            "key": "QA-2555",
-            "fields": {
-                "summary": "[QA for] password generator Android",
-                "created": "2024-08-01T19:00:26.017-0400",
-                "assignee": {
-                    "self": "https://mozilla-hub.atlassian.net/rest/api/2/user?accountId=712020%3A94a51a44-b5cc-4200-b015-b3344f6829a7", # noqa
-                    "accountId": "712020:94a51a44-b5cc-4200-b015-b3344f6829a7",
-                    "emailAddress": "dbarladeanu@mozilla.com",
-                    "avatarUrls": {
-                        "48x48": "https://avatar-management--avatars.us-west-2.prod.public.atl-paas.net/712020:94a51a44-b5cc-4200-b015-b3344f6829a7/744bb20e-d66a-475a-839a-765800c8cfda/48", # noqa
-                        "24x24": "https://avatar-management--avatars.us-west-2.prod.public.atl-paas.net/712020:94a51a44-b5cc-4200-b015-b3344f6829a7/744bb20e-d66a-475a-839a-765800c8cfda/24", # noqa
-                        "16x16": "https://avatar-management--avatars.us-west-2.prod.public.atl-paas.net/712020:94a51a44-b5cc-4200-b015-b3344f6829a7/744bb20e-d66a-475a-839a-765800c8cfda/16", # noqa
-                        "32x32": "https://avatar-management--avatars.us-west-2.prod.public.atl-paas.net/712020:94a51a44-b5cc-4200-b015-b3344f6829a7/744bb20e-d66a-475a-839a-765800c8cfda/32"  # noqa
-                    },
-                    "displayName": "Diana Andreea Barladeanu",
-                    "active": True,
-                    "timeZone": "America/Indiana/Indianapolis",
-                    "accountType": "atlassian"
-                },
-                "customfield_10155": {
-                    "value": "Fx129"
-                },
-                "customfield_10134": {
-                    "value": "Mobile Android"
-                },
-                "customfield_10037": None,
-                "status": {
-                    "name": "CI/Execution"
-                },
-                "labels": [
-                    ]
-                }
-                }
-        ]
-        '''
+
         self.db.qa_requests_delete()
 
         data_frame = self.db.report_jira_qa_requests_payload(payload)
@@ -128,6 +72,7 @@ class DatabaseJira(Database):
     def qa_requests_delete(self):
         """ Wipe out all test suite data.
         NOTE: we'll renew this data from Testrail every session."""
+        print("Delete entries from db first")
         self.session.query(ReportJiraQARequests).delete()
         self.session.commit()
 
@@ -170,6 +115,9 @@ class DatabaseJira(Database):
         # Join list of labels into a single string
         df_selected['jira_labels'] = df_selected['jira_labels'].apply(lambda x: ','.join(x) if isinstance(x, list) else x) # noqa
 
+        # Convert NaN values to 0 and ensure the column is of type int
+        df_selected['jira_story_points'] = df_selected['jira_story_points'].fillna(0).astype(int) # noqa
+
         return df_selected
 
     def report_jira_qa_requests_insert(self, payload):
@@ -186,8 +134,5 @@ class DatabaseJira(Database):
                                           jira_status=row['jira_status'], # noqa
                                           jira_assignee_username=row['jira_assignee_username'], # noqa
                                           jira_labels=row['jira_labels'])
-
-        # This is the only way working locally to insert data
-        # payload.to_sql('report_jira_qa_requests', con=engine, if_exists='append', index=False) # noqa
-        self.session.add(report)
+            self.session.add(report)
         self.session.commit()
